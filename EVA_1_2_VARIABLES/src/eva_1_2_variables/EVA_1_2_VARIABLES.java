d5c148f025ca2dfe98fcab52454537b91dfe0cec
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-08-26 17:40:30 -0600

    Add files via upload

---
+++ EVA_1_2_VARIABLES/src/eva_1_2_variables/EVA_1_2_VARIABLES.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo"); //CADENA DE TEXTO
+        System.out.println(100);          //ENTERO
+        System.out.println('c');          //NUMERO CON DECIMAL
+        System.out.println(true);         //VALORES LOGICOS (BOLEANOS)
+        System.out.println(false);
+        
+        //ENTEROS
+        //int, long, short
+        
+        //NUMERO CON DECIMAL
+        //double, float
+        
+        //CADENA DE TEXTO
+        //string
+        
+        //CARACTERES
+        //char
+        
+        //VALORES LOGICOS
+        //boolean
+        
+       
+       String nombre;
+       String apellido;
+       double promedio;
+       int edad;
+       
+       edad = 18;       
+       nombre = "DAVID ELI";
+       apellido = "GONZALEZ BALDERRAMA";
+       promedio = 8.0;
+       
+       System.out.println(edad);
+       System.out.println(nombre);
+       System.out.println(apellido);
+       System.out.println(promedio);
+       
+       
+     
+    }
+    
+}
