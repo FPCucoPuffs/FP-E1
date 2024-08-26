da45a4abf3dc456326fbcd55848ba5d7e0258230
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-08-26 17:51:30 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_variables2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int year;//la ñ en otros lenguajes no es aceptada
+        double precio;
+        
+        //ASIGNACION
+        
+        marca = "NISSAN";
+        modelo = "350z";
+        year = 2008; 
+        precio = 130000.5;
+        
+        //IMPRIMIR
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+        
+                
+           
+        
+    }
+    
+}
