fa853b6a173b8efae712fda9b1dac1331580aa04
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-09-03 17:49:59 -0600

    EVA1_10_OPERACIONES2

---
+++ EVA1_10_OPERACIONES2/src/eva1_10_operaciones2/EVA1_10_OPERACIONES2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_10_operaciones2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_OPERACIONES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //CONCATENACON
+        //OPERADOR DE CONCATENACION +
+        //MISMO SIMBOLO CON DIFERENTES USOS = COBRECARGA DE OPERADORES
+        
+        String nombre = "Eli";
+        String apellido = "Gonzalez";
+        String nomCom;
+        
+        //CONCATENAR: unir cadenaas de texto
+        nomCom = nombre + " " + apellido;
+        //imprimir los datos:
+        System.out.println("nombre del usuario: " + nombre);
+        System.out.println("apellido del usuario: " + apellido);
+        System.out.println("nombre completo: " + nomCom);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
