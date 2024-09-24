a67c5cde58000a3f31b6e302ee64fe380ae87cba
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-09-24 17:51:57 -0600

    EVA1_18_RANGOS

---
+++ EVA1_18_RANGOS/src/eva1_18_rangos/EVA1_18_RANGOS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_18_rangos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_18_RANGOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int calif;
+        Scanner captu = new Scanner(System.in);
+        
+        System.out.println("introduce tu calificacion");
+        calif = captu.nextInt();
+        if(calif >= 0 && calif <= 100){
+            System.out.println("CALIFICACION VALIDA");
+        }else{
+            System.out.println("CALIFICACION NO VALIDA");
+        }
+        }
+    
+        
+    }
+    
+}
